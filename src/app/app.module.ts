9a1d4d6958d37ed5af65474281f76241868a0773
nguyeexnnhaajtnam <n3nguyennhatnam27@gmail.com>
2023-01-28 23:41:10 +0700

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './main/navbar/navbar.component';
+import { WheelComponent } from './main/wheel/wheel.component';
+import { DiceComponent } from './main/dice/dice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent,
+    WheelComponent,
+    DiceComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
